72eff6ff8b832a9a8d5128a3f6fc5cff55d770f0
DESKTOP-375QGR3\Eimdadul Haque <eimdadulhaque@gmail.com>
2025-05-28 18:42:58 +0600

    Add_Initial_Entities migration added

--- src/Buraq.Ecommerce.EntityFrameworkCore/Migrations/20250528124045_Add_Initial_Entities.Designer.cs
+++ src/Buraq.Ecommerce.EntityFrameworkCore/Migrations/20250528124045_Add_Initial_Entities.Designer.cs
@@ -4,6 +4,7 @@ using Buraq.Ecommerce.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Volo.Abp.EntityFrameworkCore;
 
@@ -12,9 +13,11 @@ using Volo.Abp.EntityFrameworkCore;
 namespace Buraq.Ecommerce.Migrations
 {
     [DbContext(typeof(EcommerceDbContext))]
-    partial class EcommerceDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250528124045_Add_Initial_Entities")]
+    partial class Add_Initial_Entities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
