341bc377d4fae40904345ba9c4c2194c896f0544
Eimdadul Haque <eimdadulhaque@gmail.com>
2025-05-30 16:15:27 +0600

    Category Service Added

---
+++ src/Buraq.Ecommerce.Application.Contracts/Categories/DTOs/CategoryLookupDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buraq.Ecommerce.Categories.DTOs
+{
+    public class CategoryLookupDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ src/Buraq.Ecommerce.Application.Contracts/Categories/Interfaces/ICategoryAppService.cs
@@ -0,0 +1,23 @@
+﻿using Buraq.Ecommerce.Categories.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+
+namespace Buraq.Ecommerce.Categories.Interfaces
+{
+    public interface ICategoryAppService : IApplicationService
+    {
+        Task<CategoryDto> GetAsync(int id);
+        Task<CategoryWithProductsDto> GetWithProductsAsync(int id);
+        Task<PagedResultDto<CategoryDto>> GetListAsync(PagedAndSortedResultRequestDto input);
+        Task<List<CategoryLookupDto>> GetLookupListAsync();
+        Task<CategoryDto> CreateAsync(CreateUpdateCategoryDto input);
+        Task<CategoryDto> UpdateAsync(int id, CreateUpdateCategoryDto input);
+        Task DeleteAsync(int id);
+        Task<int> GetProductCountAsync(int categoryId);
+    }
+}
