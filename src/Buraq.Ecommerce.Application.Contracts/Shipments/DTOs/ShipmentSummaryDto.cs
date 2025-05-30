eb8c3b9e60a83bf6bee11dec70c065dc0cd16276
Eimdadul Haque <eimdadulhaque@gmail.com>
2025-05-30 18:39:34 +0600

    Shipment Service Added

--- src/Buraq.Ecommerce.Application.Contracts/Shipments/DTOs/ShipmentSummaryDto.cs
+++ src/Buraq.Ecommerce.Application.Contracts/Shipments/DTOs/ShipmentSummaryDto.cs
@@ -1,23 +1,21 @@
 ﻿using Buraq.Ecommerce.Enums;
-using Buraq.Ecommerce.Orders.DTOs;
-using Buraq.Ecommerce.ShipmentItems.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Volo.Abp.Application.Dtos;
 
 namespace Buraq.Ecommerce.Shipments.DTOs
 {
-    public class ShipmentDto : FullAuditedEntityDto<int>
+    public class ShipmentSummaryDto
     {
+        public int ShipmentId { get; set; }
         public int OrderId { get; set; }
+        public string OrderNumber { get; set; }
+        public ShipmentStatus Status { get; set; }
         public DateTime ShipmentDate { get; set; }
         public string Carrier { get; set; }
         public string TrackingNumber { get; set; }
-        public ShipmentStatus Status { get; set; }
-        public DateTime CreationTime { get; set; }
-        public DateTime? LastModificationTime { get; set; }
+        public int ItemCount { get; set; }
     }
 }
---
+++ src/Buraq.Ecommerce.Application.Contracts/Shipments/Interfaces/IShipmentAppService.cs
@@ -0,0 +1,27 @@
+﻿using Buraq.Ecommerce.Enums;
+using Buraq.Ecommerce.Shipments.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+
+namespace Buraq.Ecommerce.Shipments.Interfaces
+{
+    public interface IShipmentAppService : IApplicationService
+    {
+        Task<ShipmentDto> GetAsync(int id);
+        Task<ShipmentWithItemsDto> GetWithItemsAsync(int id);
+        Task<PagedResultDto<ShipmentDto>> GetListAsync(ShipmentFilterDto input);
+        Task<List<ShipmentDto>> GetByOrderAsync(int orderId);
+        Task<ShipmentDto> CreateAsync(CreateShipmentDto input);
+        Task<ShipmentDto> UpdateAsync(int id, UpdateShipmentDto input);
+        Task UpdateStatusAsync(int id, ShipmentStatus status);
+        Task AddTrackingInfoAsync(int id, string carrier, string trackingNumber);
+        Task DeleteAsync(int id);
+        Task<int> GetItemCountAsync(int shipmentId);
+        Task<ShipmentSummaryDto> GetShipmentSummaryAsync(int id);
+    }
+}
