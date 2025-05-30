5fc6b07dacaa1a2bede4386fc3c91a4da2be5492
Eimdadul Haque <eimdadulhaque@gmail.com>
2025-05-30 16:21:41 +0600

    Customer Service Added

---
+++ src/Buraq.Ecommerce.Application.Contracts/Customers/DTOs/CustomerDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+
+namespace Buraq.Ecommerce.Customers.DTOs
+{
+    public class CustomerDto : FullAuditedEntityDto<int>
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public int AddressId { get; set; }
+        public DateTime CreationTime { get; set; }
+        public DateTime? LastModificationTime { get; set; }
+    }
+}
---
+++ src/Buraq.Ecommerce.Application.Contracts/Customers/Interfaces/Interface1.cs
@@ -0,0 +1,23 @@
+﻿using Buraq.Ecommerce.Customers.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+
+namespace Buraq.Ecommerce.Customers.Interfaces
+{
+    public interface ICustomerAppService : IApplicationService
+    {
+        Task<CustomerDto> GetAsync(int id);
+        Task<CustomerWithDetailsDto> GetWithDetailsAsync(int id);
+        Task<PagedResultDto<CustomerDto>> GetListAsync(GetCustomerListDto input);
+        Task<List<CustomerLookupDto>> GetLookupListAsync();
+        Task<CustomerDto> CreateAsync(CreateUpdateCustomerDto input);
+        Task<CustomerDto> UpdateAsync(int id, CreateUpdateCustomerDto input);
+        Task DeleteAsync(int id);
+        Task<int> GetCartCountAsync(int customerId);
+    }
+}
