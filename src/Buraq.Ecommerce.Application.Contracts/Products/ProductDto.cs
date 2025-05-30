161b5b730aeda7b5a79cec4b318273fbdc9ad60c
Foyaz <ahmedfoyaz0000@gmail.com>
2025-05-30 20:09:37 +0600

    some changes

---
+++ src/Buraq.Ecommerce.Application.Contracts/Products/ProductDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+
+namespace Buraq.Ecommerce.Products
+{
+    public class ProductDto : EntityDto<int>
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public int StockQuantity { get; set; }
+        public bool IsActive { get; set; }
+        public int CategoryId { get; set; }
+        public int SupplierId { get; set; }
+        public int Image { get; set; }
+        public string? CategoryName { get; set; }
+        public string? SupplierName { get; set; }
+    }
+}
---
+++ src/Buraq.Ecommerce.Application/Products/ProductAppService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+
+namespace Buraq.Ecommerce.Products
+{
+    public class ProductAppService :
+    CrudAppService<Product, ProductDto, int, PagedAndSortedResultRequestDto, ProductDto, CreateUpdateProductDto>,
+    IProductAppService
+    {
+        public ProductAppService(
+        IRepository<Product, int> repository) : base(repository)
+        {
+
+        }
+    }
+}
